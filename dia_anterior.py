c834e83a40315b46c55ce052f7e34415caa1c83e
jzaldivar <jorge.zaldivar@gmail.com>
2024-11-01 08:45:48 -0600

    Restar uno al día

--- dia_anterior.py
+++ dia_anterior.py
@@ -1,3 +1,18 @@
 """
-Codifica tu solución en este archivo.
+Solución de Jorge
 """
+
+# Entradas
+print("Fecha inicial:")
+dia = int(input("Día: "))
+mes = int(input("Mes: "))
+anho = int(input("Año: "))
+
+# Procesos
+dia -= 1
+
+# Salidas
+print("Un día antes:")
+print("Día:", dia)
+print("Mes:", mes)
+print("Año:", anho)
